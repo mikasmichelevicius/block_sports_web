d1fe2862c90a7f0706e55b310bfa3e7ef756c74d
Mikas <mikas@Mikass-MacBook-Air.local>
2026-05-15 12:44:38 +0300

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter, Crimson_Pro, Oswald } from "next/font/google";
+import Script from "next/script";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "./globals.css";
 
@@ -37,6 +38,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BWCRW8191S"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BWCRW8191S');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${crimsonPro.variable} ${oswald.variable} antialiased`}>
         {children}
       </body>
